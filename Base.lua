55666f8e7540410659c7abcf7b26d4e7e4fb4303
Rainhard <a.borzoi@yandex.ru>
2019-06-04 14:40:56 +0300

    Create Base.lua

---
+++ Base.lua
@@ -0,0 +1,5 @@
+-- Это программа для подбора 4-ёх значного Пин-Кода
+-- Она создана чуваком по имени RAIN так что имейте уважение и в случае использования программы не забудьте про него
+-- DISCLAIMER: This program was maked only for testing your pin-code! Not for hacking!
+-- Sorry for my bad english
+
